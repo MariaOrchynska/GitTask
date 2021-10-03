72e74445a0449361d177aeeee350949637621da5
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-10-03 17:36:31 +0300

    Last commit on master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
         int b = 6;
         if (a == 0) {
             System.out.println("The result  of  this: ((" + a + b);
+            System.out.println("The commit that will be deleted  of  this: ((" + a + b);
+
 
         }
     }
