49e6620b60a76a3c463359ec7e15e3bb9696807f
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-10-03 17:12:40 +0300

    Commits on the branch alpha

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int a = 4;
+        int b = 9;
+        if (a != 0) {
+            System.out.println("The result :"+ a + b);
 
+        }
 
     }
 }
