8c11c3be9f5e99a764ac3fb4271a6fbf8efefac0
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-10-03 17:21:44 +0300

    Rebase bravo to master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int a = 0;
+        int b = 6;
+        if (a == 0) {
+            System.out.println("The result  of this: ((" + a + b);
 
-
+        }
     }
 }
